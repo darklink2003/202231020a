28752c38ec1dc069953a06161621e09d712d5656
darklink2003 <108371170+darklink2003@users.noreply.github.com>
2023-10-20 08:28:54 -0500

    vs3
    error

--- despligue.php
+++ despligue.php
@@ -1,7 +1,7 @@
 <?php
 include("funciones.php");
 //invoca la funcion consultar
-echo consultar(1);
+echo consultar('','');
 
 
 ?>
--- funciones.php
+++ funciones.php
@@ -1,31 +1,52 @@
 <?php
+// Esta función se llama consultar y se encarga de consultar y mostrar las personas de una base de datos.
+//
+// @return $texto los datos de las personas de una base de datos.
+// @param $into un numero de identificar de la persona.
 
-/**
- * Esta fucnion ConsultarPersona de encarga de consultar y mostra las personas 
- * de una base de datos.
- * @return $texto los datos de las personas de una base de datos.
- * @param $into un numero de identificar de la persona.
- */
-function consultar($u=null)
+
+function consultar($u = null, $c = null)
 {
-    $salida = ""; //inicializa la variable
-    $conexion = mysqli_connect('localhost', 'root', '', 'aa'); //establece la conexion
-    $sql = "SELECT * FROM productos"; //ejecuta el sql
+    // Declaramos una variable para almacenar la salida de la función.
+    $salida = "";
+    // Realizamos la conexión a la base de datos.
+    $conexion = mysqli_connect('localhost', 'root', '', 'aa');
+
+    // Creamos la consulta SQL para seleccionar todos los registros de la tabla `productos`.
+    $sql = "SELECT * FROM productos";
+
+    // Si se ha proporcionado un identificador de persona, lo agregamos a la consulta SQL.
+    if ($u != null) {
+        $sql .= " WHERE id = $u";
+    }
+
+    // Si se ha proporcionado una clave de persona, lo agregamos a la consulta SQL.
+    if ($c != null) {
+        $sql .= " AND clave = $c";
+    }
+
+    // Ejecutamos la consulta SQL y almacenamos el resultado en una variable.
     $resultado = mysqli_query($conexion, $sql);
 
-    if ($u!=null) $sql .=" WHERE id='$u'";
-    $resultado=$conexion->query($sql);
-
-
-    while ($fila = mysqli_fetch_assoc($resultado)) { //muestra la base de datos 
-        $salida .= $fila['id'] . " |"; //llama a la fila id
-        $salida .= $fila['nombre'] . " |"; //llama a la fila nombre
-        $salida .= $fila['clave'] . " |"; //llama a la fila clave
-        $salida .= $fila['sitio'] . " |"; //llama a la fila sitio
-        $salida .= $fila['invitacion'] . " |"; //llama a la fila invitacion
+    // Si la consulta devuelve algún resultado, lo recorremos y almacenamos los datos de cada persona en la variable `salida`.
+    if (mysqli_num_rows($resultado) > 0) {
+        while ($fila = mysqli_fetch_assoc($resultado)) {
+            $salida .= $fila['id'] . " |"; //muestra la fila id
+            $salida .= $fila['nombre'] . " |"; //muestra la fila nombre
+            $salida .= $fila['clave'] . " |"; //muestra la fila clave
+            $salida .= $fila['sitio'] . " |"; //muestra la fila sitio
+            $salida .= $fila['invitacion'] . " |"; //muestra la fila invitacion
+        }
     }
-    mysqli_close($conexion); //cierra la conexion 
-    return $salida; //retorna la variable 
+    else {
+        $salida ="eres un error";
+    }
+
+    // Cerramos la conexión a la base de datos.
+    mysqli_close($conexion);
+
+    // Devolvemos la variable `salida`.
+    return $salida;
 }
 
 
